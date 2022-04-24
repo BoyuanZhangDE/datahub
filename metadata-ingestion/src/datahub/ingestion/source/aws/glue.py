f051cdd987092ebd404fbe50f10f72b95cf731ff
BoyuanZhangDE <boyuan.zhang@onepeloton.com>
2022-04-24 18:23:15 -0400

    Update glue.py

--- metadata-ingestion/src/datahub/ingestion/source/aws/glue.py
+++ metadata-ingestion/src/datahub/ingestion/source/aws/glue.py
@@ -620,11 +620,18 @@ class GlueSource(Source):
         if self.source_config.extract_profile:
 
             # extract profile stats from glue table
-            response = self.glue_client.get_table(
-                DatabaseName=database_name,
-                Name=table_name
-                CatalogId=self.source_config.catalog_id
-            )
+            if self.source_config.catalog_id:
+                response = self.glue_client.get_table(
+                    DatabaseName=database_name,
+                    Name=table_name
+                    CatalogId=self.source_config.catalog_id
+                )
+            else:
+                response = self.glue_client.get_table(
+                    DatabaseName=database_name,
+                    Name=table_name
+                )
+
             column_stats = response['Table']['StorageDescriptor']['Columns']
             table_stats = response['Table']['Parameters']
 
